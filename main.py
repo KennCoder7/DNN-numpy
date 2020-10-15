26cc9b0aaf3d9ad66376fcb426f0f9a2adc8c2a0
Kenn <40631472+KennCoder7@users.noreply.github.com>
2020-10-15 12:54:13 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from model.Model import *
 
 def mlp_mnist():
     mnist = MNIST(dimension=3)
-    model = Model(name='model', input_dim=[1, 28, 28])
+    model = Model(name='model', input_dim=[28, 28, 1])
     model.initial(
         [
             Flatten(name='flatten'),
@@ -21,13 +21,13 @@ def mlp_mnist():
         ]
     )
     model.fit(mnist.train_x_set, mnist.train_y_set)
-    model.train(lr=0.01, momentum=0.9, max_epoch=500, batch_size=128, interval=10)
+    model.train(lr=0.1, momentum=0.9, max_epoch=500, batch_size=128, interval=10)
     print('Test_Acc=[{}]'.format(model.measure(mnist.test_x_set, mnist.test_y_set)))
 
 
 def cnn_mnist():
     mnist = MNIST(dimension=3)
-    model = Model(name='model', input_dim=[1, 28, 28])
+    model = Model(name='model', input_dim=[28, 28, 1])
     model.initial(
         [
             Conv2D(name='C1', kernel_size=[3, 3], filters=5, padding='valid'),
@@ -35,9 +35,9 @@ def cnn_mnist():
             MaxPooling2D(name='P1', pooling_size=[2, 2]),
             Flatten(name='flatten'),
             Dense(name='fc1', units=100),
-            Activation(name='A2', method='relu'),
+            Activation(name='A3', method='relu'),
             Dense(name='fc2', units=10),
-            Activation(name='A3', method='softmax'),
+            Activation(name='A4', method='softmax'),
         ]
     )
     model.fit(mnist.train_x_set, mnist.train_y_set)
@@ -45,6 +45,26 @@ def cnn_mnist():
     print('Test_Acc=[{}]'.format(model.measure(mnist.test_x_set, mnist.test_y_set)))
 
 
+def rnn_mnist():
+    mnist = MNIST(dimension=3)
+    model = Model(name='model', input_dim=[28, 28])
+    model.initial(
+        [
+            BasicRNN(name='R1', units=10, return_last_step=False),
+            Activation(name='A1', method='relu'),
+            BasicRNN(name='R2', units=10, return_last_step=False),
+            Activation(name='A2', method='relu'),
+            Flatten(name='flatten'),
+            Dense(name='fc1', units=10),
+            Activation(name='A3', method='softmax'),
+        ]
+    )
+    model.fit(mnist.train_x_set.squeeze(-1), mnist.train_y_set)
+    model.train(lr=0.01, momentum=0.9, max_epoch=500, batch_size=128, interval=10)
+    print('Test_Acc=[{}]'.format(model.measure(mnist.test_x_set.squeeze(-1), mnist.test_y_set)))
+
+
 if __name__ == '__main__':
     # mlp_mnist()
     cnn_mnist()
+    rnn_mnist()
